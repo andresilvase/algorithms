eaaaeb05a6da707cdbdb744354ac66d735cebbe7
André Felipe <academicworks.adf@gmail.com>
2024-10-11 19:46:10 -0300

    implementing hashSet and Binary Search

---
+++ miscellaneous/Kotlin/hashSet.kt
@@ -0,0 +1,31 @@
+class MyHashSet() {
+    val buckets = Array(2) { ArrayList<Int>() }
+
+    fun hashFun(key: Int): Int = key % 2
+
+    fun add(key: Int) {
+        val bucket = buckets[hashFun(key)]
+
+        if (!contains(key)) {
+            bucket.add(key)
+        }
+    }
+
+    fun remove(key: Int) {
+        val bucket = buckets[hashFun(key)]
+
+        if (contains(key)) {
+            bucket.remove(key)
+        }
+    }
+
+    fun contains(key: Int): Boolean {
+        val bucket = buckets[hashFun(key)]
+
+        return bucket.contains(key)
+    }
+}
+
+fun main() {
+    print(5 / 2)
+}
