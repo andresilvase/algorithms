c1be1cc21933e4b16061ff1e12e15b86f0439942
André Felipe <academicworks.adf@gmail.com>
2025-02-01 18:20:56 -0300

    feat: add initial implementation of a Go program with sample data

---
+++ DP/GO/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+// AMANHA PARASITA
+
+//   P A R A S I T A
+// A 0 1 1 1 1 1 1 1
+// M 0 1 1 1 1 1 1 1
+// A 0 1 1 2 2 2 2 2
+// N 0 1 1 2 2 2 2 2
+// H 0 1 1 2 2 2 2 2
+// A 0 1 1 2 2 2 2 3
+
+// PARASITA
+// AMANHA
+// AAA
+
+// PARAFERNALHA
+// PARAFUSADO
+// PARAFA
+
+// BUTECO
+// CANECO
+// ECO
+
+// XIBATA
+// CAPIXABA
+// IBA
+
+// BOTINA
+// BOTIJA
+// BOTI
+
+// CANETA
+// BOCETA
+// CETA
+
+func main() {
+	fmt.Println("Hello, World!")
+}
