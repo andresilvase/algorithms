2c644055d0b18c607e7bcce824258fa372b48f4d
André Felipe <academicworks.adf@gmail.com>
2025-02-15 14:51:33 -0300

    feat: enhance longest common subsequence function to return subsequence and length; add helper function for subsequence retrieval

--- DP/Dart/longest_common_subsequence.dart
+++ DP/Dart/longest_common_subsequence.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-int longestCommonSubsequence(String text1, String text2) {
+(String, int) longestCommonSubsequence(String text1, String text2) {
   int m = text1.length;
   int n = text2.length;
   List<List<int>> dp = List.generate(m + 1, (_) => List.filled(n + 1, 0));
@@ -14,11 +14,38 @@ int longestCommonSubsequence(String text1, String text2) {
       }
     }
   }
-  return dp[m][n];
+
+  return (showSubsequence(dp, text1, text2), dp[m][n]);
+}
+
+String showSubsequence(List<List<int>> dp, String text1, String text2) {
+  int m = text1.length;
+  int n = text2.length;
+
+  StringBuffer sub = StringBuffer();
+
+  int i = m;
+  int j = n;
+
+  while (i > 0 && j > 0) {
+    if (text1[i - 1] == text2[j - 1]) {
+      sub.write(text1[i - 1]);
+      i--;
+      j--;
+    } else {
+      if (dp[i - 1][j] > dp[i][j - 1]) {
+        i--;
+      } else {
+        j--;
+      }
+    }
+  }
+
+  return sub.toString().split('').reversed.join();
 }
 
 void main() {
-  String text1 = "AMANHA";
-  String text2 = "PARASITA";
+  String text1 = "CANETA";
+  String text2 = "BUCETA";
   print(longestCommonSubsequence(text1, text2));
 }
